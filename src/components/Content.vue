0d0d3b416280dc79e0cb9a88cd32271adaf4f350
Aidar <sharipovaydar200515@gmail.com>
2024-03-02 17:27:38 +0600

    Начал делать контейнер справа экрана, SalesMap

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="content-main-container">
-    <ContentBtns :contentIcons="contentIcons"/>
+    <div class="left-content-container">
+      <ContentBtns :contentIcons="contentIcons"/>
+  
+      <ContentTable :tableData="tableOne" />
+  
+      <ContentTable :tableData="tableTwo" />
+    </div>
 
-    <ContentTable :tableData="tableOne" />
-
-    <ContentTable :tableData="tableTwo" />
-  </div>
+    <div class="right-content-container">
+      <SalesMap />
+    </div>
+</div>
 </template>
 
 <script>
 import Icon from './MenuComponents/Icon.vue';
 import ContentBtns from "./ContentComponents/ContentBtns.vue"
 import ContentTable from './ContentComponents/ContentTable.vue';
+import SalesMap from "./ContentComponents/SalesMap.vue"
 
 export default{
   name: "Content",
   components: {
-    Icon, ContentBtns, ContentTable
+    Icon, ContentBtns, ContentTable, SalesMap
   },
   data() {
     return {
@@ -31,13 +38,13 @@ export default{
           author: "Макпал", comment: "Излишние", paymentMethod: "Kaspi макпал", sum: 40000, isNegative: false
         }, 
         {data: "01.05.2023", id: 17487, operation: "Оплата поставщику", contragent: "ТОО Форстр", checkout: "Шаляпина",
-          author: "Макпал", comment: "Излишние", paymentMethod: "Kaspi макпал", sum: 40000, isNegative: true
+          author: "Макпал", comment: "Излишние", paymentMethod: "Kaspi макпал", sum: -20000, isNegative: true
         }, 
         {data: "01.05.2023", id: 17487, operation: "Оплата поставщику", contragent: "ТОО Форстр", checkout: "Шаляпина",
           author: "Макпал", comment: "Излишние", paymentMethod: "Kaspi макпал", sum: 40000, isNegative: false
         }, 
         {data: "01.05.2023", id: 17487, operation: "Оплата поставщику", contragent: "ТОО Форстр", checkout: "Шаляпина",
-          author: "Макпал", comment: "Излишние", paymentMethod: "Kaspi макпал", sum: 40000, isNegative: true
+          author: "Макпал", comment: "Излишние", paymentMethod: "Kaspi макпал", sum: -20000, isNegative: true
         }, 
         {data: "01.05.2023", id: 17487, operation: "Оплата поставщику", contragent: "ТОО Форстр", checkout: "Шаляпина",
           author: "Макпал", comment: "Излишние", paymentMethod: "Kaspi макпал", sum: 40000, isNegative: false
@@ -45,11 +52,11 @@ export default{
       ], 
       tableTwo: [
         ["Вид оплаты", "Наличные", "Безналичные", "Всего"],
-        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222"},
-        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222"},
-        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222"},
-        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222"},
-        ["Итог", "456 665", "123 123", "4 654 456"]
+        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222", '': ''},
+        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222", '': ''},
+        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222", '': ''},
+        {paymentType: "Оплата за покупку", cash: "15 645 654", nonCash: "456 665", sum: "212 565 222", '': ''},
+        ["Итог", "456 665", "123 123", "4 654 456", '']
       ]
     }
   }
---
+++ src/components/ContentComponents/SalesMap.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="right-content-container">
+    <div class="sales-map">asdf</div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: "SalesMap",
+}
+</script>
