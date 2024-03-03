2a7dbeaadb0a101153111aceddbf51cb69b72e04
Aidar <sharipovaydar200515@gmail.com>
2024-03-03 15:19:26 +0600

    Sales map делаю

--- src/components/ContentComponents/SalesMap.vue
+++ src/components/ContentComponents/SalesMap.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="right-content-container">
-    <div class="sales-map">asdf</div>
-  </div>
+    <div class="sales-map">
+      <div class="sales-header-container">
+        <span class="sales-header-text"> {{ salesHeader }} 
+          <span class="sales-header-date"> {{ salesDates }} </span>
+        </span>
+      </div>
+
+      <div class="sales-map-elements">
+        <div class="sales-map-element">
+          <div class="sales-el-text">
+            <span class="sales-el-text"> {{ payMethod }}
+              <span class="sales-name"> {{ name }} </span>
+            </span> 
+          </div>
+
+          <div class="sales-el-value"> 
+            <Icon :name="icon"/>
+
+            <span> {{ amount }} </span>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
+import Icon from '../MenuComponents/Icon.vue';
+
 export default{
   name: "SalesMap",
+  components: {
+    Icon
+  },
+  props: {
+    // salesHeader: String,
+    // salesDates: String,
+    // payMethod: String,
+    // name: String,
+    // amount: String,
+    // icon: String
+    salesData: Array
+  }
 }
 </script>
